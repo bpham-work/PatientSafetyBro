9156b6cfe4abc564bbb11696ae626ed0013af282
Bryant Pham <Bryantphamm@gmail.com>
2014-08-20 18:17:37 -0500

    Refactored listview into a tableview

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,42 +1,63 @@
-var listView = $.list;
+var tableView = $.tableView;
+var tableData = [];
 
-var sections = [];
-
-var taskSection = Ti.UI.createListSection();
 var taskDataSet = [
-    { task: {text: 'Sedation protocol ordered?'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}},
-    { task: {text: 'Analgesia protocol ordered?'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}},
-    { task: {text: 'Neuromuscular blockade protocol ordered?'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}},
-    { task: {text: 'Delirium protocol ordered?'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}},
-    { task: {text: 'DVT prophylaxis - standard ICU orders?'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}},
-    { task: {text: 'Head of bed mechanically ventilated?'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}},
-    { task: {text: 'Head of bed >= 30 degrees?'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}},
-    { task: {text: 'Head of bed recorded on bedside chart?'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}},
-    { task: {text: 'Skin condition - Braden score recorded'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}},
-    { task: {text: 'Skin condition - Specialty bed ordered'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}},
-    { task: {text: 'Skin condition - Lesion present on transfer to ICU'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}},
-    { task: {text: 'Skin condition - Wound team consulted'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}},
-    { task: {text: 'Nutrition team assessment updated in record'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}},
-    { task: {text: 'Insulin protocol ordered?'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}},
-    { task: {text: 'If Severe Sepsis present, protocol ordered?'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}},
-    { task: {text: 'Mechanical ventilation?'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}},
-    { task: {text: 'Mechanical ventilation - Weaning protocol active?'}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}}
+    { task: 'Sedation protocol ordered?'},
+    { task:  'Analgesia protocol ordered?'},
+    { task:  'Neuromuscular blockade protocol ordered?'},
+    { task:  'Delirium protocol ordered?'},
+    { task:  'DVT prophylaxis - standard ICU orders?'},
+    { task:  'Head of bed mechanically ventilated?'},
+    { task:  'Head of bed >= 30 degrees?'},
+    { task:  'Head of bed recorded on bedside chart?'},
+    { task:  'Skin condition - Braden score recorded'},
+    { task:  'Skin condition - Specialty bed ordered'},
+    { task:  'Skin condition - Lesion present on transfer to ICU'},
+    { task:  'Skin condition - Wound team consulted'},
+    { task:  'Nutrition team assessment updated in record'},
+    { task:  'Insulin protocol ordered?'},
+    { task:  'If Severe Sepsis present, protocol ordered?'},
+    { task:  'Mechanical ventilation?'},
+    { task:  'Mechanical ventilation - Weaning protocol active?'}
 ];
-taskSection.setItems(taskDataSet);
-sections.push(taskSection);
-listView.sections[0].items = taskDataSet;
 
-function toggleCheck (e) {
-    var item = taskSection.getItemAt(e.itemIndex);
-    if (item.properties.accessoryType == Ti.UI.LIST_ACCESSORY_TYPE_NONE) {
-        item.properties.accessoryType = Ti.UI.LIST_ACCESSORY_TYPE_CHECKMARK;
-        item.template = 'checked';
-    }
-    else {
-        item.properties.accessoryType = Ti.UI.LIST_ACCESSORY_TYPE_NONE;
-        item.template = 'unchecked';
-    }
-    taskSection.updateItemAt(e.itemIndex, item);
-} 
+for(x = 0; x < taskDataSet.length; x++) {
+	var row = Ti.UI.createTableViewRow();
+	var label = Ti.UI.createLabel({
+		left: 75,
+		text: taskDataSet[x].task
+	});
+	
+	var reportButton = Ti.UI.createButton({
+		left: 0,
+		title: '!'
+	});
+
+	row.add(label);
+	row.add(reportButton);
+	
+	//We can set some row unique identifier here
+	row.rowId = x;
+	
+	label.addEventListener('click', checkRow);
+	reportButton.addEventListener('click', reportTask);
+	
+	tableData.push(row);
+}
+
+tableView.data = tableData;
+
+function checkRow(event) {
+	if(event.source.parent.hasCheck)
+		event.source.parent.hasCheck = false;
+	else
+		event.source.parent.hasCheck = true;
+}
+
+function reportTask(event) {
+	alert(event.source.parent.rowId);
+}
 
 $.index.open();
+
+
