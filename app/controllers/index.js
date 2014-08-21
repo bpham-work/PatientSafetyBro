690a20681ae330775750c525548f18ca96800483
keyneom <keyneom122@hotmail.com>
2014-08-21 10:14:40 -0500

    Merging ajax and table view

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,5 @@
-var listView = $.list;
-
-var sections = [];
-
-var taskSection = Ti.UI.createListSection();
+var tableView = $.tableView;
+var tableData = [];
 
 // Pull task data from Google Sheets Script End-point
 var ajax = Ti.Network.createHTTPClient();
@@ -14,22 +11,38 @@ var taskDataSet = [];
 ajax.onload = function(){
     Titanium.API.info(this.responseText);
     var data = this.responseText;
-    var jdata = JSON.parse(data).data;
+    var taskDataSet = JSON.parse(data).data;
     
     // Generate the required list objects for taskDataSet
     // TODO: We may want to cache this since it seems like it slows things down a bit
     // If we do it could work without a network connection and just store runs and issues locally until
     // a valid connection is established
-    for (var row in jdata)
-    {
-    	taskDataSet.push( {task: {taskId: jdata[row]['Id'], text: jdata[row]['Step']}, properties: {accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE}} );
-    }
-    
-    // Attach task data to our list view section
-	taskSection.setItems(taskDataSet);
-	sections.push(taskSection);
-	listView.sections[0].items = taskDataSet;
+    for(x = 0; x < taskDataSet.length; x++) {
+		var row = Ti.UI.createTableViewRow();
+		var label = Ti.UI.createLabel({
+			left: 75,
+			text: taskDataSet[x]['Step']
+		});
+		
+		var reportButton = Ti.UI.createButton({
+			left: 0,
+			title: '!'
+		});
 	
+		row.add(label);
+		row.add(reportButton);
+		
+		//We can set some row unique identifier here
+		row.rowId = taskDataSet[x]['Id'];
+		
+		label.addEventListener('click', checkRow);
+		reportButton.addEventListener('click', reportTask);
+	
+		tableData.push(row);
+	}
+	
+	tableView.data = tableData;
+
 	$.index.open();
 };
 ajax.open('POST', 'https://script.google.com/macros/s/AKfycbzAonjX6IGGcefEIqkC5kfG1bJ_M-RXNS6RVa80wRNM8brHNs11/exec');
@@ -37,15 +50,13 @@ ajax.send({
     "key": "991087F6-2883-11E4-896E-1D75D34483DE"
 });
 
-function toggleCheck (e) {
-    var item = taskSection.getItemAt(e.itemIndex);
-    if (item.properties.accessoryType == Ti.UI.LIST_ACCESSORY_TYPE_NONE) {
-        item.properties.accessoryType = Ti.UI.LIST_ACCESSORY_TYPE_CHECKMARK;
-        item.template = 'checked';
-    }
-    else {
-        item.properties.accessoryType = Ti.UI.LIST_ACCESSORY_TYPE_NONE;
-        item.template = 'unchecked';
-    }
-    taskSection.updateItemAt(e.itemIndex, item);
-} 
+function checkRow(event) {
+	if(event.source.parent.hasCheck)
+		event.source.parent.hasCheck = false;
+	else
+		event.source.parent.hasCheck = true;
+}
+
+function reportTask(event) {
+	alert(event.source.parent.rowId);
+}
